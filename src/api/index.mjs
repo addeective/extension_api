0b4dcf3bc725cd33e257f0297e15314b0a15e601
jm@addeective <jean-michel.lambert@addeective.com>
2024-06-23 21:29:44 +0200

    limit /api/v1/ on ip

--- src/api/index.mjs
+++ src/api/index.mjs
@@ -12,16 +12,17 @@ import emojis from './emojis.mjs'
 
 // Configure rate limiting
 const ratelimit = new Ratelimit({
-  redis:kv,
-  keyPrefix: 'ratelimit:',
-  limit: 100,  // Requests per window
-  window: 60 * 1000  // 1 minute window
-});
+  redis: kv,
+  // 5 requests from the same IP in 10 seconds
+  limiter: Ratelimit.slidingWindow(5, '10 s'),
+})
 console.log("Rate limit function is",ratelimit)
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
+  const ip = req.ip || req.connection.remoteAddress;
+  console.log("Ip="+ip);
   const { limit, reset, remaining } = await ratelimit.limit("toto")
   console.log("Toto Remaining "+remaining)
   res.json({
